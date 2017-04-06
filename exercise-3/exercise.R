f2a6dbf1bafa08012360d26ff73cc0c36ee59e89
angelalishuo <als1996@uw.edu>
2017-04-06 13:53:30 -0700

    completed exercise 3 but not bonus

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -2,18 +2,29 @@
 
 # Create a vector `marbles` with 6 different colors in it (representing marbles)
 
+marbles <- c("red", "green", "yellow", "orange", "pink", "purple")
 
 # Use the `sample` function to select a single marble
 
+marble <- sample(marbles, 1)
+  
 
 # Write a function MarbleGame that does the following:
 # - Takes in a `guess` of a marble color
 # - Randomly samples a marble
 # - Returns whether or not the person guessed accurately (preferrably a full phrase)
 
+MarbleGame <- function(guess) {
+  if (guess == marble) {
+    return ("You guessed the right marble color!")
+  } else {
+    return ("I'm sorry, you guessed the wrong marble color.")
+  }
+}
 
 # Play the marble game!
 
+MarbleGame("yellow")
 
 # Bonus: Play the marble game until you win, keeping track of how many tries you take
 
